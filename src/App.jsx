d43b874f52db9a74b5fa3e449381cb5fa26a1e1a
Wyatt May <wyattmay79@gmail.com>
2024-05-06 14:07:30 -0600

    Added 2 new components for the app, with some functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import Header from "./components/Header.jsx";
+import UserInput from "./components/Userinput.jsx";
+
+
+
 function App() {
+
+
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <Header />
+      <UserInput />
+    </>
+  );
 }
 
 export default App
