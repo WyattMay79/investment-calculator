5c9409afa3951388dbbeabcf41943ff920d0fbd5
Wyatt May <wyattmay79@gmail.com>
2024-05-06 14:46:02 -0600

    Added user input functionality as well as the calculations and display of the user inputs in a table with amounts being calculated for the user inputs

--- src/components/Userinput.jsx
+++ src/components/Userinput.jsx
@@ -1,21 +1,4 @@
-import { useState } from "react";
-
-export default function UserInput() {
-    const [userInput, setUserInput] = useState({
-        initialInvestment: 10000,
-        annualInvestment: 1200,
-        expectedReturn: 6,
-        duration: 10
-    });
-
-    function handleChange(inputIdentifier, newValue) {
-        setUserInput(prevUserInput => {
-            return {
-                ...prevUserInput,
-                [inputIdentifier]: newValue
-            };
-        });
-    }
+export default function UserInput({ onChange, userInput }) {
 
 
     return (
@@ -27,7 +10,7 @@ export default function UserInput() {
                         type="number"
                         value={userInput.initialInvestment}
                         onChange={(event) =>
-                            handleChange('initialInvestment', event.target.value)}
+                            onChange('initialInvestment', event.target.value)}
                         required
                     />
                 </p>
@@ -36,7 +19,7 @@ export default function UserInput() {
                     <input
                         type="number"
                         onChange={(event) =>
-                            handleChange('annualInvestment', event.target.value)}
+                            onChange('annualInvestment', event.target.value)}
                         value={userInput.annualInvestment}
                         required
                     />
@@ -48,7 +31,7 @@ export default function UserInput() {
                     <input
                         type="number"
                         onChange={(event) =>
-                            handleChange('expectedReturn', event.target.value)}
+                            onChange('expectedReturn', event.target.value)}
                         value={userInput.expectedReturn}
                         required
                     />
@@ -58,7 +41,7 @@ export default function UserInput() {
                     <input
                         type="number"
                         onChange={(event) =>
-                            handleChange('duration', event.target.value)}
+                            onChange('duration', event.target.value)}
                         value={userInput.duration}
                         required
                     />
